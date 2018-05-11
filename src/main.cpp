6c58475f62e7cdf1e658f73d45071e109b6fd590
Nick Enchev <nikolaenchev@gmail.com>
2018-05-11 16:56:06 -0400

    Added -d and -h directives

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,24 @@ namespace po = boost::program_options;
 
 void displayNumber(long number);
 int binSize(long number);
+void printDec(long number);
+void printHex(long number);
+void printBin(long number);
 
 int main(const int argc, const char *argv[])
 {
-    po::options_description desc("Available options");
+    po::options_description desc("Usage: nn number\nAvailable options");
 	desc.add_options()
 		("help", "See this help screen")
-		("d", "Provide a decimal number")
-		("h", "Provide a hexidecimal number")
-		("b", "Provide a binary number");
-
+		("d", po::value<long>(), "Provide a decimal number")
+		("h", po::value<string>(), "Provide a hexidecimal number")
+		("b", po::value<string>(), "Provide a binary number");
 
 	po::variables_map varmap;
 	po::store(po::parse_command_line(argc, argv, desc), varmap);
 	po::notify(varmap);
 
+	// help requested, or nn typed alone
 	if (varmap.count("help") || argc == 1)
 	{
 		cout << "numnum Version 1.0" << endl;
@@ -31,26 +34,51 @@ int main(const int argc, const char *argv[])
 	else
 	{
 		long number = 0;
-		try
+		if (varmap.count("d"))
 		{
-			number = stoi(string(argv[1]));
+			number = varmap["d"].as<long>();
+			printHex(number);
+			printBin(number);
 		}
-		catch (std::invalid_argument &ex)
+		else if (varmap.count("h"))
 		{
-			cerr << "Not a valid integer" << endl;
+			stringstream ss;
+			ss << hex << varmap["h"].as<string>();
+			ss >> number;
+			printDec(number);
+			printBin(number);
 		}
-		displayNumber(number);
+		else
+		{
+			try
+			{
+				number = stoi(string(argv[1]));
+			}
+			catch (std::invalid_argument &ex)
+			{
+				cerr << "Not a valid integer" << endl;
+			}
+		}
 	}
 	return 0;
 }
 
-void displayNumber(long number)
+void printDec(long number)
+{
+	cout << number << endl;
+}
+
+void printHex(long number)
+{
+	cout << "0x" << hex << setw(8) << setfill('0') << number << endl;
+}
+
+void printBin(long number)
 {
 	int bitCount = binSize(number);
 	if (bitCount)
 	{
 		cout << bitset<32>(number) << endl;
-		cout << "0x" << hex << setw(8) << setfill('0') << number << endl;
 	}
 }
 
