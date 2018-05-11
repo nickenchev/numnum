ab3b78d97124468d69b4f1efa08f281abe950a3e
Nick Enchev <nikolaenchev@gmail.com>
2018-05-11 16:13:21 -0400

    Added boost program_options

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,58 @@
 #include <bitset>
 #include <iostream>
 #include <iomanip>
+#include <boost/program_options.hpp>
 
 using namespace std;
+namespace po = boost::program_options;
+
+void displayNumber(long number);
+int binSize(long number);
+
+int main(const int argc, const char *argv[])
+{
+    po::options_description desc("Available options");
+	desc.add_options()
+		("help", "See this help screen")
+		("d", "Provide a decimal number")
+		("h", "Provide a hexidecimal number")
+		("b", "Provide a binary number");
+
+
+	po::variables_map varmap;
+	po::store(po::parse_command_line(argc, argv, desc), varmap);
+	po::notify(varmap);
+
+	if (varmap.count("help") || argc == 1)
+	{
+		cout << "numnum Version 1.0" << endl;
+		cout << desc << std::endl;
+	}
+	else
+	{
+		long number = 0;
+		try
+		{
+			number = stoi(string(argv[1]));
+		}
+		catch (std::invalid_argument &ex)
+		{
+			cerr << "Not a valid integer" << endl;
+		}
+		displayNumber(number);
+	}
+	return 0;
+}
+
+void displayNumber(long number)
+{
+	int bitCount = binSize(number);
+	if (bitCount)
+	{
+		cout << bitset<32>(number) << endl;
+		cout << "0x" << hex << setw(8) << setfill('0') << number << endl;
+	}
+}
 
 int binSize(long number)
 {
@@ -13,32 +63,4 @@ int binSize(long number)
 		++bitCount;
 	}
 	return bitCount;
-}
-
-int main(const int argc, const char *argv[])
-{
-	if (argc > 1)
-	{
-		int number = 0;
-		try
-		{
-			number = stoi(string(argv[1]));
-		}
-		catch (std::invalid_argument &ex)
-		{
-			cerr << "Not a valid integer" << endl;
-		}
-
-		int bitCount = binSize(number);
-		if (bitCount)
-		{
-			cout << bitset<32>(number) << endl;
-			cout << "0x" << hex << setw(8) << setfill('0') << number << endl;
-		}
-	}
-	else
-	{
-		cerr << "Specify a base 10 integer" << endl;
-	}
-	return 0;
 }
