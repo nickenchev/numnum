6b214c336a68e55acab89b0b2ab03d23a09f61d7
Nick Enchev <nikolaenchev@gmail.com>
2018-05-14 11:51:11 -0400

    Started adding new options

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 namespace po = boost::program_options;
 
-void displayNumber(long number);
+const static string appVersion = "1.0";
+
 int binSize(long number);
 void printDec(long number);
 void printHex(long number);
@@ -17,9 +18,11 @@ int main(const int argc, const char *argv[])
     po::options_description desc("Usage: nn number\nAvailable options");
 	desc.add_options()
 		("help", "See this help screen")
-		("dec,D", po::value<long>(), "Provide a decimal number")
-		("hex,H", po::value<string>(), "Provide a hexidecimal number")
-		("bin,B", po::value<string>(), "Provide a binary number");
+		("decimal,D", po::value<long>(), "Provide a decimal number")
+		("hexadecimal,H", po::value<string>(), "Provide a hexidecimal number")
+		("octal,C", po::value<string>(), "Provide an octal number")
+		("binary,B", po::value<string>(), "Provide a binary number")
+		("input,I", po::value<string>(), "Fetch input from stdin");
 
 	po::variables_map varmap;
 	po::store(po::parse_command_line(argc, argv, desc), varmap);
@@ -28,29 +31,30 @@ int main(const int argc, const char *argv[])
 	// help requested, or nn typed alone
 	if (varmap.count("help") || argc == 1)
 	{
-		cout << "numnum Version 1.0" << endl;
+		cout << "numnum Version " << appVersion << "\n" << endl;
 		cout << desc << std::endl;
 	}
 	else
 	{
 		long number = 0;
-		if (varmap.count("d"))
+		if (varmap.count("decimal"))
 		{
-			number = varmap["d"].as<long>();
-			printHex(number);
-			printBin(number);
+			number = varmap["decimal"].as<long>();
 		}
-		else if (varmap.count("h"))
+		else if (varmap.count("hexadecimal"))
 		{
 			stringstream ss;
-			ss << hex << varmap["h"].as<string>();
+			ss << hex << varmap["hexadecimal"].as<string>();
 			ss >> number;
-			printDec(number);
-			printBin(number);
 		}
 		else
 		{
 		}
+
+		// output all variants
+		printDec(number);
+		printHex(number);
+		printBin(number);
 	}
 	return 0;
 }
@@ -70,7 +74,8 @@ void printBin(long number)
 	int bitCount = binSize(number);
 	if (bitCount)
 	{
-		cout << bitset<32>(number) << endl;
+		string binStr = bitset<32>(number).to_string();
+		cout << binStr << endl;
 	}
 }
 
